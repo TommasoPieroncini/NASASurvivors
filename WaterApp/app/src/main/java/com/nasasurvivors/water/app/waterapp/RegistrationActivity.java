c0576f2b9d949bff7dbede2db7a02b956a38dbcc
ZSchlesinger <zach.schlesinger@gmail.com>
2017-02-20 03:02:41 -0500

    Nothing significant

--- WaterApp/app/src/main/java/com/nasasurvivors/water/app/waterapp/RegistrationActivity.java
+++ WaterApp/app/src/main/java/com/nasasurvivors/water/app/waterapp/RegistrationActivity.java
@@ -1,18 +1,20 @@
 package com.nasasurvivors.water.app.waterapp;
 
+import android.content.Intent;
 import android.net.Credentials;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by zachschlesinger on 2/19/17.
  */
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    private CredentialVerification creds = new CredentialVerification();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +23,18 @@ public class RegistrationActivity extends AppCompatActivity {
 
         EditText user = (EditText) findViewById(R.id.email_input);
         EditText pass = (EditText) findViewById(R.id.password_input);
+
+        final String userStr = user.getText().toString();
+        final String passStr = pass.getText().toString();
+
         Button registerBtn = (Button) findViewById(R.id.register_btn);
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                creds.addCreds(userStr, passStr);
+                Intent registered = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(registered);
+                Toast.makeText(getBaseContext(), "Registered!", Toast.LENGTH_SHORT).show();
             }
         });
     }
