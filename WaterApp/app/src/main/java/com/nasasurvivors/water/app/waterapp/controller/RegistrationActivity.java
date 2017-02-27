9caf62414c0406ff20f28c1677c73633306420aa
ZSchlesinger <zach.schlesinger@gmail.com>
2017-02-27 01:03:42 -0500

    Add firebase registration functionality

--- WaterApp/app/src/main/java/com/nasasurvivors/water/app/waterapp/controller/RegistrationActivity.java
+++ WaterApp/app/src/main/java/com/nasasurvivors/water/app/waterapp/controller/RegistrationActivity.java
@@ -1,8 +1,11 @@
 package com.nasasurvivors.water.app.waterapp.controller;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -10,6 +13,10 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.nasasurvivors.water.app.waterapp.R;
 import com.nasasurvivors.water.app.waterapp.model.AppSingleton;
 import com.nasasurvivors.water.app.waterapp.model.CredentialVerification;
@@ -31,6 +38,12 @@ public class RegistrationActivity extends AppCompatActivity {
     private EditText email;
     private Spinner typeSpinner;
     private Button registerBtn;
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +57,9 @@ public class RegistrationActivity extends AppCompatActivity {
         pass = (EditText) findViewById(R.id.password_input);
         name = (EditText) findViewById(R.id.name_input);
         email = (EditText) findViewById(R.id.email_input);
+        progressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         // Populate user types spinner
         List<UserType> userTypes = Arrays.asList(UserType.USER, UserType.WORKER,
@@ -53,7 +69,6 @@ public class RegistrationActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         typeSpinner.setAdapter(adapter);
         typeSpinner.setPrompt("Select your user type");
-
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +83,7 @@ public class RegistrationActivity extends AppCompatActivity {
                 if (CredentialVerification.getInstance().validateUsername(user, userStr) &&
                 CredentialVerification.getInstance().validatePass(pass, passStr) &&
                 CredentialVerification.getInstance().validateEmail(email, emailStr)) {
-
+                    registerUser(emailStr, passStr);
                     // Create user
                     User newUser = new User(userStr, passStr, nameStr, emailStr, type);
 
@@ -83,6 +98,8 @@ public class RegistrationActivity extends AppCompatActivity {
                         // Set new user
                         AppSingleton.getInstance().setCurrentUser(newUser);
 
+
+
                         registered.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(registered);
 
@@ -94,5 +111,24 @@ public class RegistrationActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void registerUser(String email, String password) {
+        progressDialog.setMessage("Registering...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getBaseContext(), "Successful", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e("firebase issue", task.getException().getMessage());
+                            Toast.makeText(getBaseContext(), "Not successful", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
